4e13bf853bbf113eeab4d75f25db37c97376255f
unknown <GAS@.(none)>
2011-06-04 13:31:03 +1000

    - Инициальная структура программы для руководства.

--- QuestMain.hs
+++ QuestMain.hs
@@ -1,44 +1,6 @@
-module Main where
 
-import Types
-import Locations
-import System.IO (hFlush, stdout)
-import Control.Monad.State (liftIO)
-
-directionsToString :: [Direction] -> String
-directionsToString (dir:dirs) = show dir ++ case null(dirs) of
-												True -> []
-												False -> ", " ++ directionsToString dirs
-directionsToString [] = []
-
-describeDirections :: Location -> String
-describeDirections loc = "You can go " ++ directionsToString ( getLocationDirections loc)
-
-describeActions :: Location -> String
-describeActions _ = "You can do something."
-
-describeGameSituation :: Location -> String
-describeGameSituation loc = do
-						describeDirections loc
-						describeActions loc
-
-
-getAction = do
-				putStr "> "
-				hFlush stdout
-				line <- getLine
-				return (line)
-
-run :: Location -> IO ()
-run oldLoc = do
-				putStrLn . describeLocation $ oldLoc
-				putStrLn . describeDirections $ oldLoc
-				putStrLn . describeActions $ Room
-				x <- getAction
-				case x of
-					"Quit" -> return ()
-					"q" -> return ()
-					otherwise -> run . getLocation $ (oldLoc, x)
-			
-main :: IO ()
-main = run Room
+describeLocation :: String -> String
+describeLocation locName = case locName of
+            "Home" -> "You are standing in the middle room at the wooden table."
+            "Friend's home" -> "You are standing in the front of the night garden behind the small wooden fence."
+            otherwise -> "Unknown location."

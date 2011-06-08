e59195606846dcf7d1b3c194679042c4ccf89556
Granin Alexandr <graninas@gmail.com>
2011-06-08 14:25:50 +0900

    - Обновлен код для второй части.

--- QuestMain.hs
+++ QuestMain.hs
@@ -1,17 +1,24 @@
 
-data Location = Home
+data Location =
+		  Home
 		| Friend'sYard
 		| Garden
 		| OtherRoom
-	deriving (Eq, Show, Read)
+	deriving (Eq)
 
-data Direction = North | South | West | East
+data Direction =
+			  North
+			| South
+			| West
+			| East
+	deriving (Eq)
 
 data Action = Look | Go | Inventory | Take | Drop | Investigate | Quit | Save | Load | New
+    deriving (Eq)
 		
 describeLocation :: Location -> String
 describeLocation loc = case loc of
-            Home -> "You are standing in the middle room at the wooden table."
+            Home         -> "You are standing in the middle room at the wooden table."
             Friend'sYard -> "You are standing in the front of the night garden behind the small wooden fence."
-            Garden -> "You are in the garden. Garden looks very well: clean, tonsured, cool and wet."
-            otherwise -> "Unknown location."
+            Garden       -> "You are in the garden. Garden looks very well: clean, tonsured, cool and wet."
+            otherwise    -> "Unknown location."

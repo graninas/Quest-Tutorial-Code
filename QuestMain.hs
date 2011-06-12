d883017836b000366ac2ac8ee07c4a4909886e37
Granin Alexandr <graninas@gmail.com>
2011-06-13 00:19:04 +0900

    - QuestMain.hs для текущей версии части3.

--- QuestMain.hs
+++ QuestMain.hs
@@ -4,21 +4,50 @@ data Location =
 		| Friend'sYard
 		| Garden
 		| OtherRoom
-	deriving (Eq)
+	deriving (Eq, Show)
 
 data Direction =
 			  North
 			| South
 			| West
 			| East
-	deriving (Eq)
+	deriving (Eq, Show)
 
-data Action = Look | Go | Inventory | Take | Drop | Investigate | Quit | Save | Load | New
-    deriving (Eq)
-		
+data Action =
+		  Look
+		| Go
+		| Inventory
+		| Take
+		| Drop
+		| Investigate
+		| Quit 
+		| Save 
+		| Load 
+		| New
+    deriving (Eq, Show)
+
 describeLocation :: Location -> String
-describeLocation loc = case loc of
+describeLocation loc = show loc ++ "\n" ++
+		case loc of
             Home         -> "You are standing in the middle room at the wooden table."
             Friend'sYard -> "You are standing in the front of the night garden behind the small wooden fence."
             Garden       -> "You are in the garden. Garden looks very well: clean, tonsured, cool and wet."
-            otherwise    -> "Unknown location."
+            otherwise    -> "No description available for location with name " ++ show loc ++ "."
+
+-- Обрабатываем действие.
+evalAction :: Action -> String
+evalAction act = case act of
+			Look -> "Action: Look!"
+			otherwise -> "Action does not supported yet."
+
+-- Преобразуем строку в Action
+convertStringToAction :: String -> Action
+convertStringToAction str = case str of
+		"Look"    -> Look
+		otherwise -> Quit
+
+-- Получем ввод с клавиатуры, конвертируем его в действие, вызываем обработчик, выводим результат.
+run = do
+		putStr "Enter command: "
+		x <- getLine
+		putStrLn ( evalAction (convertStringToAction x) )
